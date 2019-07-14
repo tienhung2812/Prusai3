b68ce6569ad161c2f60b7c263f4090cfb8ff873b
Nguyen Tien Hung <hung.nguyentien97>
2019-07-14 17:04:11 +0700

    Adjust bed

--- Marlin-1.0.x/Marlin-1.0.x/Marlin/Configuration.h
+++ Marlin-1.0.x/Marlin-1.0.x/Marlin/Configuration.h
@@ -386,8 +386,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
     // set the rectangle in which to probe
     #define LEFT_PROBE_BED_POSITION 0
-    #define RIGHT_PROBE_BED_POSITION 170
-    #define BACK_PROBE_BED_POSITION 180
+    #define RIGHT_PROBE_BED_POSITION 150
+    #define BACK_PROBE_BED_POSITION 150
     #define FRONT_PROBE_BED_POSITION 0
 
      // set the number of grid points per dimension
